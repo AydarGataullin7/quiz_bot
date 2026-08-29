d66809f9fb139e69904a10ecf989fa0364c1f064
AydarGataullin7 <aydarcr7@gmail.com>
2026-08-29 12:17:34 +0500

    improve code structure

--- questions_parser.py
+++ questions_parser.py
@@ -1,39 +1,47 @@
 import os
 import json
 
-questions_folder = r'D:\python_scripts\quiz_bot\questions'
-all_files = os.listdir(questions_folder)
+QUESTIONS_FOLDER = r'D:\python_scripts\quiz_bot\questions'
 
-all_questions = {}
 
-for questions_file in all_files:
-    full_path = questions_folder + "\\" + questions_file
-    with open(full_path, 'r', encoding='koi8-r') as file:
-        content = file.read()
+def main():
+    all_files = os.listdir(QUESTIONS_FOLDER)
+    all_questions = {}
 
-    blocks = content.split('\n\n')
+    for questions_file in all_files:
+        full_path = QUESTIONS_FOLDER + "\\" + questions_file
+        with open(full_path, 'r', encoding='koi8-r') as file:
+            content = file.read()
 
-    i = 0
-    while i < len(blocks):
-        block = blocks[i].strip()
+        blocks = content.split('\n\n')
 
-        if block.startswith('Вопрос'):
-            question_text = block
+        i = 0
+        while i < len(blocks):
+            block = blocks[i].strip()
 
-            answer_text = None
-            j = i + 1
-            while j < len(blocks):
-                if blocks[j].strip().startswith('Ответ'):
-                    answer_text = blocks[j].replace('Ответ:', '').strip()
-                    break
-                j += 1
+            if block.startswith('Вопрос'):
+                question_text = block
 
-            if question_text and answer_text:
-                all_questions[question_text] = answer_text
+                answer_text = None
+                j = i + 1
+                while j < len(blocks):
+                    if blocks[j].strip().startswith('Ответ'):
+                        answer_text = blocks[j].replace('Ответ:', '').strip()
+                        break
+                    j += 1
 
-            i = j + 1
-        else:
-            i += 1
+                if question_text and answer_text:
+                    all_questions[question_text] = answer_text
 
-with open('questions.json', 'w', encoding='utf-8') as f:
-    json.dump(all_questions, f, ensure_ascii=False, indent=2)
+                i = j + 1
+            else:
+                i += 1
+
+    with open('questions.json', 'w', encoding='utf-8') as f:
+        json.dump(all_questions, f, ensure_ascii=False, indent=2)
+
+    print(f"Найдено вопросов: {len(all_questions)}")
+
+
+if __name__ == "__main__":
+    main()
